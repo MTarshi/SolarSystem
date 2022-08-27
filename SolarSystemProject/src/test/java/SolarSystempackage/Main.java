90c252025ca52f3f7356221b3ac777109722b13d
MTarshi <106284081+MTarshi@users.noreply.github.com>
2022-08-27 12:29:52 -0400

    second commit

---
+++ SolarSystemProject/src/test/java/SolarSystempackage/Main.java
@@ -0,0 +1,10 @@
+package SolarSystempackage;
+
+public class Main {
+
+    public static void main(String[] args) {
+        //write your code here
+
+        SolarSystem inception = new SolarSystem();
+    }
+}
